2291f0fe1d955e5e28f37a6600d6fa1e40a34323
Adrian Vargas <adrian.vargas@uao.edu.co>
2023-02-20 18:43:50 -0500

    cambios en el index servidor y creacion de los archivos con las rutas del crud de citas

---
+++ server/controllers/citas.controllers.js
@@ -0,0 +1,19 @@
+export const getCitas = (req, res)=>{
+    res.send('obteniendo citas')
+}
+
+export const getCita = (req, res)=>{
+    res.send('obteniendo una cita')
+}
+
+export const createCita = (req, res)=>{
+    res.send('creando citas')
+}
+
+export const updateCita = (req, res)=>{
+    res.send('actualizando cita')
+}
+
+export const deleteCita = (req, res)=>{
+    res.send('eliminando cita')
+}
---
+++ server/routes/citas.routes.js
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import {
+  getCita,
+  getCitas,
+  createCita,
+  deleteCita,
+  updateCita,
+} from "../controllers/citas.controllers.js";
+const router = Router();
+
+router.get("/citas", getCitas);
+
+router.get("citas/:id", getCita);
+
+router.post("/citas", createCita);
+
+router.put("citas/:id", updateCita);
+
+router.delete("citas/:id", deleteCita);
+
+export default router;
