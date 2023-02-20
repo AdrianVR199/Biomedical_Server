7f86f2877464ba3297244b0ce41acc9c20454cc0
Adrian Vargas <adrian.vargas@uao.edu.co>
2023-02-20 18:14:08 -0500

    cambios en el index servidor y creacion de la conexion a la base de datos

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from "express";
+import {PORT} from "./config.js"
+
+import indexRoutes from './routes/index.routes.js'
 
 const app = express();
-
-app.listen(3000);
-console.log("Server is running on port 3000");
+app.use(indexRoutes)
+app.listen(PORT);
+console.log(`Server is running on port ${PORT}`);
