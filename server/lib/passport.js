54ba3dadc1b5949c479672fcad72fea51b86815e
Adrian Vargas <adrian.vargas@uao.edu.co>
2023-06-20 11:35:24 -0500

    prueba auth4

--- server/lib/passport.js
+++ server/lib/passport.js
@@ -32,6 +32,7 @@ passport.use(
           user.usuario_id = fromBinaryUUID(user.usuario_id);
 
           done(null, user);
+          console.log(user, "usuario login")
         } else {
           const info = 1;
           done(null, false, { message: "Contraseña incorrecta" });
@@ -136,10 +137,13 @@ passport.use(
 );
 
 passport.serializeUser((user, done) => {
+  console.log(user, "usuario serialize")
   done(null, user.usuario_id);
 });
 
 passport.deserializeUser(async (usuario_id, done) => {
+  console.log(usuario_id, "usuario serialize")
+
   const ddd = "6c951ea0-c43b-11ed-b78e-27beb8bef137";
   const rows = await pool.query(
     "SELECT BIN_TO_UUID(u.usuario_id) as usuario_id, u.nombre_completo, u.correo, u.contraseña, u.tipo_identificacion, u.num_identificacion, u.genero, u.fecha_nacimiento, u.num_tel_celular, u.num_tel_fijo,u.direccion, u.nacionalidad, u.id_tipo_usuario, u.id_imagen,u.fecha_crea_usuario , u.id_ciudad_nac,u.id_ciudad_resi, c.nombre_ciudad as ciudad_nac,  d.nombre_departamento as dep_nac,d.departamento_id as dep_nac_id, p.nombre_pais as pais_nac, c2.nombre_ciudad as ciudad_res, d2.nombre_departamento as depa_res, d2.departamento_id as depa_res_id, p2.nombre_pais as pais_res FROM usuarios u JOIN ciudades c ON u.id_ciudad_nac=c.ciudad_id JOIN departamentos d ON c.id_departamento=d.departamento_id JOIN paises p ON d.id_pais=p.pais_id JOIN ciudades c2 ON u.id_ciudad_resi=c2.ciudad_id JOIN departamentos d2 ON c2.id_departamento=d2.departamento_id JOIN paises p2 ON d2.id_pais=p2.pais_id   WHERE BIN_TO_UUID(usuario_id)=?",
