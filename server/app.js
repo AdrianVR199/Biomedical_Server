9f6828a10c675b5a00183e8fbce6bd530768a76d
Adrian Vargas <adrian.vargas@uao.edu.co>
2023-06-21 11:14:37 -0500

    prueba auth29

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ import { db } from "./keys.js";
 const app = express();
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 app.use(
