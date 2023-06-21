5b0e922abaf0bda904b2c3b65cee0e938cfe2ea2
Adrian Vargas <adrian.vargas@uao.edu.co>
2023-06-21 16:14:27 -0500

    serv2

--- server/keys.js
+++ server/keys.js
@@ -2,24 +2,24 @@ import { Router } from "express";
 
 const router = Router();
 
-export const db = {
-  database: {
-    connectionLimit: 10,
-    host: "localhost",
-    user: "root",
-    password: "12345",
-    database: "biomedical_group_app",
-  },
-};
-
 // export const db = {
 //   database: {
 //     connectionLimit: 10,
-//     host: "database-biomedical-g.ctve6mdscvkw.us-east-1.rds.amazonaws.com",
-//     user: "admin",
-//     password: "BMG1999.dbweb",
+//     host: "localhost",
+//     user: "root",
+//     password: "12345",
 //     database: "biomedical_group_app",
 //   },
 // };
+
+export const db = {
+  database: {
+    connectionLimit: 10,
+    host: "database-biomedical-g.ctve6mdscvkw.us-east-1.rds.amazonaws.com",
+    user: "admin",
+    password: "BMG1999.dbweb",
+    database: "biomedical_group_app",
+  },
+};
 
 export default router;
