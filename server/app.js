107f958588f3bc82c569e610ca0f9c6a8c667a07
Adrian Vargas <adrian.vargas@uao.edu.co>
2023-06-28 15:25:18 -0500

    cambio serverr 14

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "*",
+    origin:'http://localhost:3000',
     methods: "GET, POST, PUT, DELETE",
     allowedHeaders: "Content-Type, Authorization",
     credentials: true,
