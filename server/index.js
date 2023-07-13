483937912677b70600aa99e2919cde79eeac7d76
Adrian Vargas <adrian.vargas@uao.edu.co>
2023-07-13 15:07:26 -0500

    server 2try6

--- server/index.js
+++ server/index.js
@@ -2,7 +2,5 @@ import app from "./app.js";
 import { PORT } from "./config.js";
 
 app.listen(PORT);
-app.get('/.well-known/pki-validation/8CE71567C23CF4B61C412616C1775A5A.txt'), (req,res)=>{
-    res.sendFile('../8CE71567C23CF4B61C412616C1775A5A.txt')
-  }
+
 console.log(`Server is running on port ${PORT}`);
