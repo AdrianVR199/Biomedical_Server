25fdf0585b45696d315a81298a4f882e2a8904f7
Adrian Vargas <adrian.vargas@uao.edu.co>
2023-07-13 15:19:00 -0500

    server 2try9

--- server/index.js
+++ server/index.js
@@ -3,7 +3,5 @@ import { PORT } from "./config.js";
 
 app.listen(PORT);
 
-app.get('/.well-known/pki-validation/8CE71567C23CF4B61C412616C1775A5A.txt'), (req,res)=>{
-    res.sendFile('/home/ubuntu/project/Biomedical_Server/8CE71567C23CF4B61C412616C1775A5A.txt')
-  }
+
 console.log(`Server is running on port ${PORT}`);
